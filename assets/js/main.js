2aabe3b5c6896ab0d97ab366caac5449295298b1
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2022-04-10 09:16:45 +0200

    Changes in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,54 +7,6 @@ window.onscroll = () => {
   }
 }
 
-
-
-/*
-let test1 = document.querySelector('.test-1');
-let opinion1 = document.querySelector('.opinion-1');
-
-test1.onclick = function() {
-  opinion1.classList.toggle('block');
-}
-
-let test2 = document.querySelector('.test-2');
-let opinion2 = document.querySelector('.opinion-2');
-
-test2.onclick = function() {
-  opinion2.classList.toggle('block');
-}
-
-let test3 = document.querySelector('.test-3');
-let opinion3 = document.querySelector('.opinion-3');
-
-test3.onclick = function() {
-  opinion3.classList.toggle('block');
-}
-
-let test4 = document.querySelector('.test-4');
-let opinion4 = document.querySelector('.opinion-4');
-
-test4.onclick = function() {
-  opinion4.classList.toggle('block');
-}
-
-let test5 = document.querySelector('.test-5');
-let opinion5 = document.querySelector('.opinion-5');
-
-test5.onclick = function() {
-  opinion5.classList.toggle('block');
-}
-
-
-let test6 = document.querySelector('.test-6');
-let opinion6 = document.querySelector('.opinion-6');
-
-test6.onclick = function() {
-  opinion6.classList.toggle('block');
-}
-*/
-
-
 const bengoBtns = document.querySelectorAll('.test');
 const opinions = document.querySelectorAll('.opinion');
 
@@ -67,11 +19,3 @@ bengoBtns.forEach((btn) => {
     })
   });
 });
-
-/* $('.finiture-wrapper').on('click', function() {
-  //first hide all other div
-  $(".allestimento-img-wrapper").hide();
-  var idBtn = $(this).data('id');
-  //now open the selected div
-  $("#div_"+idBtn).css('display', 'flex');
-}) */
